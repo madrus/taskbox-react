6c6a342cfaa3bd15aaa63ed3b4b6c4635e9270ba
Andre Roussakoff <madrus@gmail.com>
2020-08-09 13:45:42 +0200

    added addon-docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,8 @@
 module.exports = {
 	addons: [
+		'@storybook/addon-actions/register',
+		'@storybook/addon-knobs/register',
+		'@storybook/addon-links/register',
 		'@storybook/preset-create-react-app',
 		{
 			name: '@storybook/addon-docs',
